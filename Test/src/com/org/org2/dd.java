88f290b4f81ff3fcbd384595f015fd44e7f3fc60
김창회 <kopr3963@naver.com>
2017-01-20 00:34:49 +0900

    commit1

--- Test/src/com/org/org2/dd.java
+++ Test/src/com/org/org2/dd.java
@@ -2,6 +2,16 @@ package com.org.org2;
 
 public class dd {
 	public static void main(String[] args) {
+		int num = 1;
+		int str = 1;
+		
+		String str1 = "1";
+		String str2 = "1";
+		String str3 = new String("1");
 
+
+		System.out.println(str1.equals(str3));
+		System.out.println(str1==str3);
+		
 	}
 }
