56b02994d5ef629853e9267425acae1666f03db9
김창회 <kopr3963@naver.com>
2017-01-20 01:32:35 +0900

    d

--- Test/src/dddd.java
+++ Test/src/dddd.java
@@ -6,10 +6,7 @@ public class dddd {
 		System.out.println("commit2");
 		System.out.println("commit3");
 		System.out.println("revert1");
-<<<<<<< HEAD
 		System.out.println("revert2");
 		System.out.println("revert3");
-=======
->>>>>>> parent of 7978e06... revert2
 	}
 }
