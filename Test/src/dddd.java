9cb3300f282a6a3da9cf889a8f57312a412e3039
김창회 <kopr3963@naver.com>
2017-01-20 01:41:24 +0900

    Revert "d"
    This reverts commit 2a9d6c4b07bdcf2bdb599a1415dee91f45d31aae.

--- Test/src/dddd.java
+++ Test/src/dddd.java
@@ -6,5 +6,16 @@ public class dddd {
 		System.out.println("commit2");
 		System.out.println("commit3");
 		System.out.println("revert1");
+<<<<<<< HEAD
+		System.out.println("revert2");
+<<<<<<< HEAD
+<<<<<<< HEAD
+		System.out.println("revert3");
+=======
+>>>>>>> parent of 7978e06... revert2
+=======
+>>>>>>> parent of 236a3c0... revert3
+=======
+>>>>>>> parent of 236a3c0... revert3
 	}
 }
